acda8963bdb9892e2580379254c82b54239f3068
Gary Wu <s19003045@gmail.com>
2019-09-03 23:58:18 +0800

    Add route

--- app.js
+++ app.js
@@ -23,11 +23,39 @@ db.once('open', () => {
 const Todo = require('./models/todo')
 
 
-// 
+// route setting
 app.get('/', (req, res) => {
   res.send('hello world. Who are you?')
 })
 
+// 列出全部 Todo
+app.get('/todos', (req, res) => {
+  res.send('列出所有 Todo')
+})
+// 新增一筆 Todo 頁面
+app.get('/todos/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+// 顯示一筆 Todo 的詳細內容
+app.get('/todos/:id', (req, res) => {
+  res.send('顯示 Todo 的詳細內容')
+})
+// 新增一筆  Todo
+app.post('/todos', (req, res) => {
+  res.send('建立 Todo')
+})
+// 修改 Todo 頁面
+app.get('/todos/:id/edit', (req, res) => {
+  res.send('修改 Todo 頁面')
+})
+// 修改 Todo
+app.post('/todos/:id/edit', (req, res) => {
+  res.send('修改 Todo')
+})
+// 刪除 Todo
+app.post('/todos/:id/delete', (req, res) => {
+  res.send('刪除 Todo')
+})
 
 // server listen
 app.listen(port, () => {
