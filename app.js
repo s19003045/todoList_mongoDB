0c60307b416a7bed56ce32df8a85f1fa77f9188e
Gary Wu <s19003045@gmail.com>
2019-09-05 10:42:36 +0800

    Add delete action

--- app.js
+++ app.js
@@ -55,16 +55,6 @@ app.get('/todos/new', (req, res) => {
   res.render('new')
 })
 
-// 顯示一筆 Todo 的詳細內容
-app.get('/todos/:id', (req, res) => {
-
-  Todo.findById(req.params.id, (err, todo) => {
-    if (err) console.err(err)
-    return res.render('detail', { todo: todo })
-  })
-
-})
-
 // 新增一筆  Todo
 app.post('/todos', (req, res) => {
   // 以 document 的方法處理"新增""
@@ -81,15 +71,26 @@ app.post('/todos', (req, res) => {
   // })
 
   // 以 model 的方法處理"新增"：
-  Todo.create({ name: req.body.name }, (err, too) => {
+  Todo.create({ name: req.body.name }, (err, todo) => {
     if (err) return console.err(err)
     return res.redirect('/')
   })
 })
 
+// 顯示一筆 Todo 的詳細內容
+app.get('/todos/:id', (req, res) => {
+
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.err(err)
+    return res.render('detail', { todo: todo })
+  })
+
+})
+
+
+
 // 修改 Todo 頁面
 app.get('/todos/:id/edit', (req, res) => {
-
   Todo.findById(req.params.id, (err, todo) => {
     if (err) console.err(err)
     return res.render('edit', { todo, todo })
@@ -98,9 +99,8 @@ app.get('/todos/:id/edit', (req, res) => {
 
 // 修改 Todo
 app.post('/todos/:id/edit', (req, res) => {
-
   Todo.findById(req.params.id, (err, todo) => {
-    if (err) console.error(err)
+    if (err) return console.error(err)
     todo.name = req.body.name
     todo.save(err => {
       if (err) return console.error(err)
@@ -111,8 +111,15 @@ app.post('/todos/:id/edit', (req, res) => {
 
 // 刪除 Todo
 app.post('/todos/:id/delete', (req, res) => {
-  res.send('刪除 Todo')
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.error(err)
+    todo.remove(err => {
+      if (err) return console.error(err)
+      return res.redirect('/')
+    })
+  })
 })
+
 
 // server listen
 app.listen(port, () => {
